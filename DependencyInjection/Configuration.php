78a08f3c925dd2a71c4cf6f4da41aa1b7532b9e0
Yasmany Cubela <ycubela@smartappsla.com>
2013-07-01 16:39:47 -0430

    fixing configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -16,10 +16,10 @@ class Configuration implements ConfigurationInterface
         ->arrayNode( 'functions_name' )->canBeUnset( )->addDefaultsIfNotSet()->children( ) // childrens
         ->scalarNode( 'name' )->cannotBeEmpty( )->defaultValue( 'setFirstname' )->end( ) // first name
         ->scalarNode( 'lastname' )->cannotBeEmpty( )->defaultValue( 'setLastname' )->end( ) // last name
-        ->scalarNode( 'lastname2' )->defaultValue( 'setLastname2' )->addDefaultsIfNotSet()->end( ) // last name 2
+        ->scalarNode( 'lastname2' )->defaultValue( 'setLastname2' )->end( ) // last name 2
         ->scalarNode( 'email' )->cannotBeEmpty( )->defaultValue( 'setEmail' )->end( ) // email
         ->scalarNode( 'username' )->cannotBeEmpty( )->defaultValue( 'setUsername' )->end( ) // username
-        ->scalarNode( 'photo' )->defaultValue( 'setPhoto' )->addDefaultsIfNotSet()->end( ) // photo
+        ->scalarNode( 'photo' )->defaultValue( 'setPhoto' )->end( ) // photo
         ->end( )->end( ) // end array
         ->scalarNode( 'default_group' )->cannotBeEmpty( )->defaultValue( 'USER' )->end( ) // default group
         ->booleanNode( 'set_group_as_social_name' )->cannotBeEmpty( )->defaultValue( true )->end( ) // group as social name
