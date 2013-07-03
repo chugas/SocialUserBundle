606bbff59b540243b3283487afe76314415d3ddd
Yasmany Cubela <ycubela@smartappsla.com>
2013-07-03 13:42:20 -0430

    adding funciton to set fb access token

--- Controller/SocialUserController.php
+++ Controller/SocialUserController.php
@@ -3,12 +3,36 @@
 namespace BIT\SocialUserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class SocialUserController extends Controller
 {
   
+  /**
+   * Lists all User entities.
+   *
+   * @Route("/login_check", name="bit_socialuser_login_check")
+   * @Method("GET")
+   */
+  
   public function checkAction( )
   {
     return new Response( '');
+  }
+  
+  /**
+   * Lists all User entities.
+   *
+   * @Route("/setFBAccessToken", name="bit_socialuser_set_fb_at")
+   */
+  
+  public function setFBAccessTokenAction( )
+  {
+    $accessToken = $this->getRequest( )->get( "accessToken" );
+    $this->get( "session" )->set( "fb.accessToken", $accessToken );
+    $this->get( "bit_facebook.api" )->setAccessToken( $accessToken );
+    return new JsonResponse( array( "accessToken" => $accessToken ));
   }
 }
