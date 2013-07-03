608d0d81abb71c53be528a4b607053a93a1a4cc2
Yasmany Cubela <ycubela@smartappsla.com>
2013-07-03 16:31:39 -0430

    fixing controller

--- Controller/SocialUserController.php
+++ Controller/SocialUserController.php
@@ -3,6 +3,7 @@
 namespace BIT\SocialUserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
