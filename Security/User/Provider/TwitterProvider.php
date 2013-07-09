54d2f59049b177ddd2728b7d43b1ff0577140468
Yasmany Cubela Medina <yasmanycm@gmail.com>
2013-07-09 07:05:04 -0430

    readding twitter

---
+++ Security/User/Provider/TwitterProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace BIT\SocialUserBundle\Security\User\Provider;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Validator\Validator;
+use FOS\UserBundle\Doctrine\UserManager;
+use BIT\SocialUserBundle\Controller\SocialUserControllerService;
+use BIT\SocialUserBundle\Security\User\Provider\SocialUserProvider;
+use BIT\SocialUserBundle\Entity\User as SocialUser;
+use BIT\TwitterBundle\Twitter\TwitterSessionPersistence;
+
+class TwitterProvider extends SocialUserProvider
+{
+  /**
+   * @var \Twitter
+   */
+  protected $twitter;
+  protected $session;
+  
+  public function __construct( TwitterSessionPersistence $twitter, Validator $validator, Session $session,
+      UserManager $userManager, SocialUserControllerService $socialUserManager )
+  {
+    parent::__construct( $validator, $userManager, $socialUserManager );
+    $this->session = $session;
+    $this->twitter = $twitter;
+    $this->providerName = "Twitter";
+  }
+  
+  protected function getData( )
+  {    
+    $data = array( );
+    
+    try
+    {
+      $info = $this->twitter->account_verifyCredentials( );
+      $data[ 'id' ] = strtolower( $info->id );
+      
+      if ( isset( $info->name ) )
+      {
+        $nameAndLastNames = explode( " ", $info->name );
+        $data[ 'firstname' ] = $nameAndLastNames[ 0 ];
+      
+        if ( count( $nameAndLastNames ) > 1 )
+          $data[ 'lastname' ] = $nameAndLastNames[ 1 ];
+      
+        if ( count( $nameAndLastNames ) > 2 )
+          $data[ 'lastname2' ] = $nameAndLastNames[ 2 ];
+      }
+      
+      $data[ 'email' ] = sprintf( "%s@%s.com", $data[ 'id' ], strtolower( $this->providerName ) );
+      $data[ 'username' ] = $info->screen_name;
+      
+      $data[ 'photo' ] = $info->profile_image_url;
+    }
+    catch ( Exception $e )
+    {
+      $data = null;
+    }
+    
+    return $data;
+  }
+}
